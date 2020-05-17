e7938f35e6e69ca03a36f240c0200cb7cda2fc80
Glitch (fuchsia-salt-inspiration) <none>
2020-05-17 05:11:39 +0000

    ⛽️🌷 Updated with Glitch

---
+++ eco/removemoney.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports.run = async (bot, message, args) => {  
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply({ 
+embed:{ 
+title: "only Admin parmission user use this Command.", 
+color: 0xff0000
+}});
+
+  let user = message.mentions.members.first() || message.author;
+
+    if (isNaN(args[1])) return;
+    db.subtract(`money_${message.guild.id}_${user.id}`, args[1])
+    let bal = await db.fetch(`money_${message.guild.id}_${user.id}`)
+
+    let moneyEmbed = new Discord.MessageEmbed()
+    .setColor("#FFFFFF")
+    .setDescription(`💸 | Removed ${args[1]} coins\n\nNew Balance: ${bal}`);
+    message.channel.send(moneyEmbed)
+
+};
+
+
+module.exports.help = {
+  name:"remove",
+  aliases: ["rm"]
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+const Discord = require("discord.js"),
+path = require("path"); 
+const fs = require("fs");
+
+module.exports = {
+
+  version: require("./package.json").version,
+
+  start(client, prefix){
+    if(!client){
+            throw new Error("No client found.");
+        }
+    if(!prefix) return console.log("[economy-uplife]⚠️:{please give me prefix}");
+  const bot = client;
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./eco/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+   /* console.log("Couldn't find commands.");*/
+    return;
+  }
+  
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./eco/${f}`);
+    /*console.log(`${f} loaded!`);*/
+    bot.commands.set(props.help.name, props);
+    props.help.aliases.forEach(alias => { 
+      bot.aliases.set(alias, props.help.name);
+  
+  });
+});
+})
+bot.on("ready", async () => {
+ /* console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setActivity(`In Development`);
+  bot.user.setStatus('online');
+*/
+  bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+  
+    let messageArray = message.content.split(" ");
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+    let commandfile;
+
+    if (bot.commands.has(cmd)) {
+      commandfile = bot.commands.get(cmd);
+  } else if (bot.aliases.has(cmd)) {
+    commandfile = bot.commands.get(bot.aliases.get(cmd));
+  }
+  
+      if (!message.content.startsWith(prefix)) return;
+
+          
+  try {
+    commandfile.run(bot, message, args);
+  
+  } catch (e) {
+  }}
+  )})
+}
+
+}
