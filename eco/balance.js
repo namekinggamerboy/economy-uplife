1e9c7edd7697f63d1527ab3e67111b5799c8fc37
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-05-17 11:22:06 +0530

    Update balance.js
    Fix

--- eco/balance.js
+++ eco/balance.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 
-module.exports.run = async (bot, message, args, utils) => {
+module.exports.run = async (bot, message, args) => {
 
   let user = message.mentions.members.first() || message.author;
 
