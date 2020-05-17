a3bde395bf730a2d551319e8288ebfc535998c8f
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-05-17 11:30:51 +0530

    Update leaderboard.js
    Leaderboard update..

--- eco/leaderboard.js
+++ eco/leaderboard.js
@@ -4,7 +4,14 @@ const db = require('quick.db')
 module.exports.run = async (bot, message, args) => {
 let client = bot;
 
-    let title = 'Leaderboards';
+const embed = new Discord.MessageEmbed()
+    .setDescription(`**Input a Leaderboard Option**\n\nCoin Leaderboard: <prefix>leaderboard coins\nbank leaderboard: <prefix>leaderboard bank`)
+    .setColor("#FF0000");
+
+  if(!args[0]) return message.channel.send(embed)
+
+    if (args[0] == 'coins') {
+
   var finalLb = "";
   db.startsWith(`money_${message.guild.id}`, { sort: '.data'}).then(resp => {
       resp.length = 15;
@@ -19,10 +26,32 @@ let client = bot;
     const embed = new Discord.MessageEmbed()
     .setAuthor(`${message.guild.name} - Leaderboard!`, message.guild.iconURL())
     .setDescription(finalLb)
-    .setColor(0x51267)
+    .setColor(0x00ff00)
 
     message.channel.send(embed)
   });
+}
+    if (args[0] == 'banks') {
+  var finalLb = "";
+  db.startsWith(`bank_${message.guild.id}`, { sort: '.data'}).then(resp => {
+      resp.length = 15;
+
+      let i = 0;
+      for (i in resp) {
+        if (resp[i] == null || resp[i] == undefined) {
+          db.set(`bank_${message.guild.id}_${resp[i].ID.split('_')[2]}`, 50);
+        }
+        finalLb += `**${client.users.get(resp[i].ID.split('_')[2]).username}** - \`$${resp[i].data}\`\n`;
+      }
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`${message.guild.name} - Leaderboard!`, message.guild.iconURL())
+    .setDescription(finalLb)
+    .setColor(0x00ff00);
+
+    message.channel.send(embed)
+  });
+}
+
 }
 module.exports.help = {
   name:"leaderboard",
