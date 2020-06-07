b0a1cfbfcc2eb04fe16e50637488124b6f2a506f
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-06-07 15:07:32 +0530

    Update buy.js

--- eco/buy.js
+++ eco/buy.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
     let author = db.fetch(`money_${message.guild.id}_${user.id}`)
 
     let Embed = new Discord.MessageEmbed()
-    .setColor("#FFFFFF")
-    .setDescription(`✅ | You need 2000 coins to purchase Bronze VIP`);
+    .setColor("#FF0000")
+    .setDescription(`❌ | You need 2000 coins to purchase Bronze VIP`);
 
     if (args[0] == 'bronze') {
         if (author < 3500) return message.channel.send(Embed)
@@ -18,15 +18,15 @@ module.exports.run = async (bot, message, args) => {
         db.set(`bronze_${message.guild.id}_${user.id}`, true)
 
         let Embed2 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
+        .setColor("#00FF00")
         .setDescription(`✅ | Purchased Bronze VIP For 3500 Coins`);
 
         db.subtract(`money_${message.guild.id}_${user.id}`, 3500)
         message.channel.send(Embed2)
     } else if(args[0] == 'nikes') {
         let Embed2 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
-        .setDescription(`✅ | You need 600 coins to purchase some Nikes`);
+        .setColor("#FF0000")
+        .setDescription(`❌ | You need 600 coins to purchase some Nikes`);
 
         if (author < 600) return message.channel.send(Embed2)
        
@@ -34,15 +34,15 @@ module.exports.run = async (bot, message, args) => {
         db.add(`nikes_${message.guild.id}_${user.id}`, 1)
 
         let Embed3 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
+        .setColor("#00FF00")
         .setDescription(`✅ | Purchased Fresh Nikes For 600 Coins`);
 
         db.subtract(`money_${message.guild.id}_${user.id}`, 600)
         message.channel.send(Embed3)
     } else if(args[0] == 'car') {
         let Embed2 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
-        .setDescription(`✅ | You need 800 coins to purchase a new car`);
+        .setColor("#FF0000")
+        .setDescription(`❌ | You need 800 coins to purchase a new car`);
 
         if (author < 800) return message.channel.send(Embed2)
        
@@ -50,15 +50,15 @@ module.exports.run = async (bot, message, args) => {
         db.add(`car_${message.guild.id}_${user.id}`, 1)
 
         let Embed3 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
+        .setColor("#00FF00")
         .setDescription(`✅ | Purchased a New Car For 800 Coins`);
 
         db.subtract(`money_${message.guild.id}_${user.id}`, 800)
         message.channel.send(Embed3)
     } else if(args[0] == 'mansion') {
         let Embed2 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
-        .setDescription(`✅ | You need 1200 coins to purchase a Mansion`);
+        .setColor("#FF0000")
+        .setDescription(`❌ | You need 1200 coins to purchase a Mansion`);
 
         if (author < 1200) return message.channel.send(Embed2)
        
@@ -66,14 +66,14 @@ module.exports.run = async (bot, message, args) => {
         db.add(`house_${message.guild.id}_${user.id}`, 1)
 
         let Embed3 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
+        .setColor("#00FF00")
         .setDescription(`✅ Purchased a Mansion For 1200 Coins`);
 
         db.subtract(`money_${message.guild.id}_${user.id}`, 1200)
         message.channel.send(Embed3)
     } else {
         let embed3 = new Discord.MessageEmbed()
-        .setColor("#FFFFFF")
+        .setColor("#FF0000")
         .setDescription('❌ | Enter an item to buy')
         message.channel.send(embed3)
     }
