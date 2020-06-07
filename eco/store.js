4142af691ae4823c70c2eed9725df74f693b0b6c
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-06-07 15:23:52 +0530

    Update store.js

--- eco/store.js
+++ eco/store.js
@@ -4,8 +4,8 @@ const db = require('quick.db')
 module.exports.run = async (bot, message, args) => {
 
     let embed = new Discord.MessageEmbed()
-    .setDescription("**VIP Ranks**\n\nBronze: 3500 Coins [m!buy bronze]\n\n**Lifestyle Items**\n\nFresh Nikes: 600 [m!buy nikes]\nCar: 800 [m!buy car]\nMansion: 1200 [m!buy mansion]")
-    .setColor("#FFFFFF")
+    .setDescription("**VIP Ranks**\n\nBronze: 3500 Coins [<prefix>buy bronze]\n\n**Lifestyle Items**\n\nFresh Nikes: 600 [<prefix>buy nikes]\nCar: 800 [<prefix>buy car]\nMansion: 1200 [<prefix>buy mansion]")
+    .setColor("#0099ff")
     message.channel.send(embed)
 
 
