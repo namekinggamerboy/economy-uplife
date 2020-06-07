13699e30aeb5df217cfdad11e06aa6063059c94d
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-06-07 15:32:45 +0530

    Update beg.js

--- eco/beg.js
+++ eco/beg.js
@@ -3,46 +3,32 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
+
   let user = message.author;
-  let amount = args[0];
 
-let embed2 = new Discord.MessageEmbed()
-  .setColor("#FF0000")
-  .setDescription(`❌ | Specify an amount to deposit`);
+  let timeout = 180000;
+  let amount = Math.floor(Math.random() * 30) + 1;
+ 
+
+  let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
+
+  if (beg !== null && timeout - (Date.now() - beg) > 0) {
+    let time = ms(timeout - (Date.now() - beg));
   
-  if (!args[0]) {
-      return message.channel.send(embed2)
-      .catch(err => console.log(err))
-  }
-
-let money = await db.fetch(`money_${message.guild.id}_${user.id}`)
-let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
-
-if(args[0] === "all"){
-let embedbank = new Discord.MessageEmbed()
-    .setColor('#FF0000')
-    .setDescription("❌ | You don't have any money to beg")
-
-    if(bank === 0) return message.channel.send(embedbank)
-
-    db.add(`money_${message.guild.id}_${user.id}`, bank)
-    db.subtract(`bank_${message.guild.id}_${user.id}`, bank)
-    let embed5 = new Discord.MessageEmbed()
-  .setColor("#00FF00")
-  .setDescription(`✅ | You have beg all your coins into your poket`);
-  message.channel.send(embed5);
-
-} else {
+    let timeEmbed = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setDescription(`❌ | You've already begged recently\n\nBeg again in ${time.minutes}m ${time.seconds}s `);
+    message.channel.send(timeEmbed)
+  } else {
     let moneyEmbed = new Discord.MessageEmbed()
   .setColor("#00FF00")
-  .setDescription(`✅ | You have beg your **${amount}** coins into your poket`);
+  .setDescription(`✅ | You've begged and received ${amount} coins`);
   message.channel.send(moneyEmbed)
-  db.add(`money_${message.guild.id}_${user.id}`, args[0])
-  db.subtract(`bank_${message.guild.id}_${user.id}`, args[0])
- 
-}
+  db.add(`money_${message.guild.id}_${user.id}`, amount)
+  db.set(`beg_${message.guild.id}_${user.id}`, Date.now())
 
- 
+
+  }
 };
 
 
