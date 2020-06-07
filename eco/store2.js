006e3c6e816cbc13b359da7eda5a1058412d342d
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-06-07 15:25:30 +0530

    Update store2.js

--- eco/store2.js
+++ eco/store2.js
@@ -6,22 +6,22 @@ module.exports.run = async (bot, message, args) => {
     
       let embed = new Discord.MessageEmbed()
       .setDescription("**Bronze Rank**\n\nBenefits: Chance to get more coins from robbing someone")
-      .setColor("#FFFFFF")
+      .setColor("#FFFF00")
       message.channel.send(embed)
     } else if(args[0] == 'nikes') {
       let embed = new Discord.MessageEmbed()
       .setDescription("**Fresh Nikes**\n\nBenefits: Chance to win coins, roles on our Discord Server + More by leading the leaderboard")
-      .setColor("#FFFFFF")
+      .setColor("#FFFF00")
       message.channel.send(embed)
     } else if(args[0] == 'car') {
       let embed = new Discord.MessageEmbed()
       .setDescription("**Car**\n\nBenefits: Chance to win coins, roles on our Discord Server + More by leading the leaderboard")
-      .setColor("#FFFFFF")
+      .setColor("#FFFF00")
       message.channel.send(embed)
   } else if(args[0] == 'mansion') {
     let embed = new Discord.MessageEmbed()
     .setDescription("**Mansion**\n\nBenefits: Chance to win coins, roles on our Discord Server + More by leading the leaderboard")
-    .setColor("#FFFFFF")
+    .setColor("#FFFF00")
     message.channel.send(embed)
   }
 
