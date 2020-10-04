32e67c9adc7618c4700533c04425d28ca53eef7d
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-10-04 13:40:49 +0530

    Update balance.js

--- eco/balance.js
+++ eco/balance.js
@@ -5,17 +5,17 @@ module.exports.run = async (bot, message, args) => {
 
   let user = message.mentions.users.first() || message.author;
 
-  let bal = await db.fetch(`money_${message.guild.id}_${user.id}`)
+  let bal = db.get(`money_${message.guild.id}_${user.id}`)
 
   if (bal === null) bal = 0;
 
-  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+  let bank = db.get(`bank_${message.guild.id}_${user.id}`)
   if (bank === null) bank = 0;
 
   let moneyEmbed = new Discord.MessageEmbed()
   .setColor("#FFD300")
   .setThumbnail(user.displayAvatarURL()+"?size=4096")
-  .setDescription(`**${user}'s Balance**\n\nPocket: ${bal}\nBank: ${bank}`);
+  .setDescription(`**${user}'s Balance**\n\nPocket: ${bal}\nBank: ${bank}\nTotal Balance:${bal+bank}`);
   message.channel.send(moneyEmbed)
 };
 
